f52fcb6890743ab8f43c5e7922899d69e0835d97
Gaz <gaz@Gazs-MacBook-Pro.local>
2024-11-04 16:06:58 +0000

    revert previous change.

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_BASE_PATH: isProduction ? "/dr-mo-teeth" : "",
   },
-  trailingSlash: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "/dr-mo-teeth/" : "",
 };
 
 export default nextConfig;
