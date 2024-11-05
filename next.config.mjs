fed28a692d594454b437f9a9be1ee600ceb6e308
Gaz <gaz@Gazs-MacBook-Pro.local>
2024-11-05 03:34:11 +0000

    reintroduce basepath config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 
-// const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
-// const nextConfig = {
-//   basePath: isProduction ? "/dr-mo-teeth" : "",
-//   output: "export",
-//   env: {
-//     NEXT_PUBLIC_BASE_PATH: isProduction ? "/dr-mo-teeth" : "",
-//   },
-// };
+const nextConfig = {
+  basePath: isProduction ? "/dr-mo-teeth" : "",
+  output: "export",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProduction ? "/dr-mo-teeth" : "",
+  },
+};
 
-// export default nextConfig;
+export default nextConfig;
