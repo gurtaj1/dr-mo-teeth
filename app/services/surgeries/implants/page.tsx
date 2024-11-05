58544befcc3eab1fa71546e5cfef548c41d6ee51
Gaz <gaz@Gazs-MacBook-Pro.local>
2024-11-05 03:19:41 +0000

    remove unused useEffect imports.

--- app/services/surgeries/implants/page.tsx
+++ app/services/surgeries/implants/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 import { useIntersectionObservers } from "@/hooks/useIntersectionObservers";
 
