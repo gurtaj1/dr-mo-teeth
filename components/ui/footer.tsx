9d07f91adcd38387125fe326986ad1291ff149c1
Gaz <gaz@Gazs-MacBook-Pro.local>
2024-11-07 09:13:42 +0000

    udpate footer.

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="bg-dental-black text-dental-accent1 pt-12 pb-12">
       <div className="container mx-auto px-6">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
             <h3 className="text-lg font-semibold mb-4">Quick Links</h3>
             <ul className="space-y-2">
@@ -22,43 +22,6 @@ const Footer = () => {
               </li>
               <li>
                 <Link href="#book">Book Now</Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="text-lg font-semibold mb-4">Services</h3>
-            <ul className="space-y-2">
-              <li>
-                <Link
-                  href="/services/teeth-whitening"
-                  className="hover:text-dental-accent2"
-                >
-                  Teeth Whitening
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/services/dental-implants"
-                  className="hover:text-dental-accent2"
-                >
-                  Dental Implants
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/services/invisalign"
-                  className="hover:text-dental-accent2"
-                >
-                  Invisalign
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/services/veneers"
-                  className="hover:text-dental-accent2"
-                >
-                  Veneers
-                </Link>
               </li>
             </ul>
           </div>
@@ -80,15 +43,17 @@ const Footer = () => {
             <ul className="space-y-2">
               <li className="flex items-center">
                 <Phone className="mr-2" size={18} />
-                <span>(123) 456-7890</span>
+                <span>01482 898362 / 01482 871704</span>
               </li>
               <li className="flex items-center">
                 <Mail className="mr-2" size={18} />
-                <span>info@drmoteeth.com</span>
+                <span>implantclinician@outlook.com</span>
               </li>
               <li className="flex items-center">
                 <MapPin className="mr-2" size={18} />
-                <span>123 Dental St, City, State 12345</span>
+                <Link href="https://maps.app.goo.gl/dbgXNq8s5xRRJCPW7">
+                  10 Soutter Gate, Hedon, Hull HU12 8JS
+                </Link>
               </li>
             </ul>
             <Button asChild className="mt-4">
