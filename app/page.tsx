31a5b01210d18115944e754e3252d53da7430864
Gaz <gaz@Gazs-MacBook-Pro.local>
2024-11-10 12:07:37 +0000

    explicitly declare text color of homepage title hero.

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ const HomePage = () => {
         </video>
         <div className="absolute inset-0 bg-dental-black bg-opacity-60"></div>
         <div className="relative z-10 text-center ">
-          <h1 className="font-newsreader text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold mb-4">
+          <h1 className="font-newsreader text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold mb-4 text-white">
             DR. MO
           </h1>
           <Button
